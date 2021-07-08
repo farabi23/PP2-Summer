d7fda9e936567dd4d6451139d4d5e72173a1bb02
farabi23 <78350272+farabi23@users.noreply.github.com>
2021-07-08 13:08:00 +0600

    LECTURE6

---
+++ Week2/lecture3/6.py
@@ -0,0 +1,12 @@
+def count_case(s):
+    cntl = 0
+    cntu = 0
+    for i in s:
+        if i.isupper():
+            cntu += 1
+        if i.islower():
+            cntl += 1
+    return cntu, cntl
+s = input()
+print(f'No. of Upper case characters: {count_case(s)[0]}\nNo. of Lower case Characters: {count_case(s)[1]}')
+© 2021 GitHub, Inc.
